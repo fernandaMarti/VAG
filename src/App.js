dce53152de64cd3263437002df2d900fe5b578e0
unknown <jaggermarti80@gmail.com>
2023-03-15 19:55:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ItemListContainer from './components/ItemListContainer/Index';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <>
+    <Navbar />
+    <ItemListContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ItemListContainer/Index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Title from "../Title/Index";
+export const ItemListContainer = () => {
+    return (
+        <Title greeting= 'Contenido' />
+    );
+}
+export default ItemListContainer;
